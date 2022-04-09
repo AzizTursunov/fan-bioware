ea5809ef2bb8892340c56169c05f1d8e29c4d534
Aziz Tursunov <az.tursunov@gmail.com>
2022-04-09 17:52:26 +0300

    Add tests to project
    Create tests dir in bioware app. Include bioware.urls to project urls
    and add urls tests.

---
+++ fanbioware/bioware/tests/test_urls.py
@@ -0,0 +1,22 @@
+from django.test import TestCase, Client
+from http import HTTPStatus
+
+
+class URLTest(TestCase):
+    def setUp(self) -> None:
+        self.guest_client = Client()
+        self.urls_vs_templates = {
+            '/': 'bioware/index.html',
+            '/about/': 'boiware/about.html',
+            '/games/': 'bioware/games.html',
+            '/games/mass_effect/': 'bioware/mass_effect.html',
+            '/careers/': 'bioware/careers.html',
+            '/contacts/': 'bioware/contacts.html'
+        }
+
+    def test_urls_exists_at_desired_location(self):
+        """Checking the availability of pages to an anonymous user."""
+        for url in self.urls_vs_templates:
+            response = self.guest_client.get(url)
+            with self.subTest(response=response):
+                self.assertEqual(response.status_code, HTTPStatus.OK)
