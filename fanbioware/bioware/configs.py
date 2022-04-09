a36032ecafce840b70407457496efbe0548f38a3
Aziz Tursunov <az.tursunov@gmail.com>
2022-04-09 18:15:02 +0300

    Create configs.py and put urls with templates in it

---
+++ fanbioware/bioware/configs.py
@@ -0,0 +1,8 @@
+URL_VS_TEMPLATE = {
+    '/': 'bioware/index.html',
+    '/about/': 'boiware/about.html',
+    '/games/': 'bioware/games.html',
+    '/games/mass_effect/': 'bioware/mass_effect.html',
+    '/careers/': 'bioware/careers.html',
+    '/contacts/': 'bioware/contacts.html'
+}
--- fanbioware/bioware/tests/test_urls.py
+++ fanbioware/bioware/tests/test_urls.py
@@ -1,29 +1,22 @@
 from django.test import TestCase, Client
 from http import HTTPStatus
+from ..configs import URL_VS_TEMPLATE
 
 
 class URLTest(TestCase):
     def setUp(self) -> None:
         self.guest_client = Client()
-        self.urls_vs_templates = {
-            '/': 'bioware/index.html',
-            '/about/': 'boiware/about.html',
-            '/games/': 'bioware/games.html',
-            '/games/mass_effect/': 'bioware/mass_effect.html',
-            '/careers/': 'bioware/careers.html',
-            '/contacts/': 'bioware/contacts.html'
-        }
 
     def test_urls_exists_at_desired_location(self):
         """Checking the availability of pages to an anonymous user."""
-        for url in self.urls_vs_templates:
+        for url in URL_VS_TEMPLATE:
             response = self.guest_client.get(url)
             with self.subTest(response=response):
                 self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_pages_uses_correct_templates(self):
         """Checking that URLs use the appropriate templates."""
-        for url, template in self.urls_vs_templates.items():
+        for url, template in URL_VS_TEMPLATE.items():
             response = self.guest_client.get(url)
             with self.subTest(response=response):
                 self.assertTemplateUsed(response, template)
