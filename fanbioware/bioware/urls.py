3c3266da0ff63c83459598a33c76b3639be6fb87
Aziz Tursunov <az.tursunov@gmail.com>
2022-04-18 21:57:44 +0300

    Set identation 4 spaces in urls.py

--- fanbioware/bioware/urls.py
+++ fanbioware/bioware/urls.py
@@ -6,16 +6,16 @@ from . import views
 app_name = 'bioware'
 
 urlpatterns = [
-     path('', views.index, name='index'),
-     path('about/', views.about, name='about'),
-     path('games/', views.game_list, name='game_list'),
-     path('games/<slug:game_slug>/', views.game_detail, name='game_detail'),
-     path('careers/', views.careers, name='careers'),
-     path('contacts/', views.contacts, name='contacts'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('games/', views.game_list, name='game_list'),
+    path('games/<slug:game_slug>/', views.game_detail, name='game_detail'),
+    path('careers/', views.careers, name='careers'),
+    path('contacts/', views.contacts, name='contacts'),
 ]
 
 if settings.DEBUG is True:
     urlpatterns += static(
-         settings.MEDIA_URL,
-         document_root=settings.MEDIA_ROOT
-     )
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
