17fac339359dde0e4a9c9c5f47c680f0927e9bce
Aziz Tursunov <az.tursunov@gmail.com>
2022-04-21 15:27:42 +0300

    Rename and add context into mass-effect-legendary-edition  template, revise view function

--- fanbioware/bioware/views.py
+++ fanbioware/bioware/views.py
@@ -37,8 +37,15 @@ def game_list(request):
 def game_detail(request, game_slug):
     template = f'bioware/{game_slug}.html'
     game_title = ' '.join([word.capitalize() for word in game_slug.split('-')])
+    game = get_object_or_404(Game, slug=game_slug)
+    news = News.objects.filter(game=game)[:4]
+    main_news = news.first()
+    news_list = news[1:]
     context = {
-        'title': game_title
+        'title': game_title,
+        'game': game,
+        'main_news': main_news,
+        'news_list': news_list
     }
     return render(request, template, context)
 
