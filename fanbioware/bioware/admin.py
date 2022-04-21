543b047392bb825a3c5b045bd2f8d5ebb17de40d
Aziz Tursunov <az.tursunov@gmail.com>
2022-04-21 08:00:00 +0300

    Set models configurations in admin

--- fanbioware/bioware/admin.py
+++ fanbioware/bioware/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
-from .models import Game, News, Studio, Opening
+from .models import Game, News, Studio, Opening, GameSliderImage, StudioSliderImage
+
+
+class GameSliderImageInline(admin.TabularInline):
+    model = GameSliderImage
+    extra = 5
+
+
+class StudioSliderImageInline(admin.TabularInline):
+    model = StudioSliderImage
+    extra = 4
 
 
 class GameAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title', )}
+    inlines = [GameSliderImageInline, ]
+
+
+class StudioAdmin(admin.ModelAdmin):
+    inlines = [StudioSliderImageInline, ]
 
 
 admin.site.register(Game, GameAdmin)
 admin.site.register(News)
-admin.site.register(Studio)
+admin.site.register(Studio, StudioAdmin)
 admin.site.register(Opening)
