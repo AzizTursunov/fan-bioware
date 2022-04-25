f996cc2c67855558fb8d1f7d53c13c188b3d09d7
Aziz Tursunov <az.tursunov@gmail.com>
2022-04-25 07:39:13 +0300

    Register new app for common features

--- fanbioware/fanbioware/settings.py
+++ fanbioware/fanbioware/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bioware.apps.BiowareConfig',
+    'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
