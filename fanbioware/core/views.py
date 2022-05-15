d310de3656597c182c83fdee9b55e3b6747a0e54
Aziz Tursunov <az.tursunov@gmail.com>
2022-05-15 14:55:13 +0300

    Create view functions for 404 and 500 errors pages.

--- fanbioware/core/views.py
+++ fanbioware/core/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def page_not_found(request, exception):
+    context = {
+        'code': 404,
+        'message': 'Oops, the page you’re looking for can’t be found.'
+    }
+    return render(
+        request,
+        'core/errors.html',
+        context,
+        status=404,
+    )
+
+
+def server_error(request):
+    context = {
+        'code': 500,
+        'message': 'Internal server error. Try again after a while.'
+    }
+    return render(
+        request,
+        'core/errors.html',
+        context,
+        status=500,
+    )
