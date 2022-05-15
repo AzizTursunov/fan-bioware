831b16a0e4cf388b242e4b8088629fc31c06943d
Aziz Tursunov <az.tursunov@gmail.com>
2022-05-15 14:57:28 +0300

    Revise urls files, add handler404 and handler500 to project urls.

--- fanbioware/bioware/urls.py
+++ fanbioware/bioware/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from . import views
 
 app_name = 'bioware'
@@ -17,9 +15,3 @@ urlpatterns = [
     path('careers/', views.CareersView.as_view(), name='careers'),
     path('contacts/', views.ContactsView.as_view(), name='contacts'),
 ]
-
-if settings.DEBUG is True:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
