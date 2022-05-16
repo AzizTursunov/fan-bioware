96529605a2005207e3ccd832643b49abec57537f
Aziz Tursunov <az.tursunov@gmail.com>
2022-05-16 21:07:39 +0300

    Create test for page not found case.

--- fanbioware/core/tests.py
+++ fanbioware/core/tests.py
@@ -1,3 +1,12 @@
+from http import HTTPStatus
 from django.test import TestCase
 
-# Create your tests here.
+
+class CoreViewTest(TestCase):
+    def test_unexisting_url_not_exists(self):
+        """Check that request to /unexisting-page/ return 404 error and
+        template core/errors.html will be used.
+        """
+        response = self.client.get('/unexisting-page/')
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        self.assertTemplateUsed(response, 'core/errors.html')
