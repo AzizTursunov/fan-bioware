0c859a9981cf88cfefe5c6768484a613ee5b2288
Aziz Tursunov <az.tursunov@gmail.com>
2022-05-16 21:11:55 +0300

    Remove news model from bioware app.

--- fanbioware/bioware/models.py
+++ fanbioware/bioware/models.py
@@ -1,4 +1,3 @@
-from datetime import date
 from multiselectfield import MultiSelectField
 from django.db import models
 from django.urls import reverse
@@ -123,47 +122,6 @@ class Game(models.Model):
             )
             platforms.append(console_text)
         return platforms
-
-
-class News(models.Model):
-    """Creating the News model."""
-
-    game = models.ForeignKey(
-        Game, on_delete=models.CASCADE, blank=True,
-        null=True, related_name='news',
-        verbose_name='Game',
-        help_text='Choose the game',
-    )
-    title = models.CharField(
-        max_length=45,
-        verbose_name='Title',
-        help_text='Enter a title of the news'
-    )
-    pub_date = models.DateField(default=date.today())
-    intro = models.CharField(
-        max_length=90,
-        verbose_name='Intro',
-        help_text='Enter a short intro of the news',
-    )
-    text = models.TextField(
-        verbose_name='Text',
-        help_text='Enter the news text'
-    )
-    image = models.ImageField(
-        verbose_name='Cover',
-        help_text='Attach the cover of the news',
-        upload_to='bioware/news/',
-    )
-    is_publicated = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = ['-pub_date']
-        verbose_name = 'News'
-        verbose_name_plural = 'News'
-
-    def __str__(self):
-        """Return the title of the News."""
-        return self.title
 
 
 class Opening(models.Model):
