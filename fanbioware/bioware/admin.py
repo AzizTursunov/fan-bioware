2c8f7ce20eead4f1c712ce580abdf2dea11340a7
Aziz Tursunov <az.tursunov@gmail.com>
2022-05-18 18:25:52 +0300

    Delete news admin, configue Game, Opening and Studio admin.

--- fanbioware/bioware/admin.py
+++ fanbioware/bioware/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from bioware.models import Game, Studio, Opening, GameSliderImage, StudioSliderImage
-from news.models import News
+
 
 class GameSliderImageInline(admin.TabularInline):
     model = GameSliderImage
@@ -15,13 +15,26 @@ class StudioSliderImageInline(admin.TabularInline):
 class GameAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title', )}
     inlines = [GameSliderImageInline, ]
+    list_display = ('id', 'title', 'image', 'description', 'is_released')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'description', 'genre')
 
 
 class StudioAdmin(admin.ModelAdmin):
     inlines = [StudioSliderImageInline, ]
+    list_display = (
+        'id', 'location', 'address1',
+        'phone', 'email', 'description'
+    )
+    list_display_links = ('id', 'location')
+
+
+class OpeningAdmin(admin.ModelAdmin):
+    list_display = ('id', 'role', 'team', 'studio', 'is_opened')
+    list_display_links = ('id', 'role')
+    search_fields = ('role', 'team', 'description', 'studio__location')
 
 
 admin.site.register(Game, GameAdmin)
-admin.site.register(News)
 admin.site.register(Studio, StudioAdmin)
-admin.site.register(Opening)
+admin.site.register(Opening, OpeningAdmin)
