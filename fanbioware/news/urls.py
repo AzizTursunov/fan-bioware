bf57017a1aa695ac8071eb105736745d8f330827
Aziz Tursunov <az.tursunov@gmail.com>
2022-05-18 18:12:22 +0300

    Revise news urls.

--- fanbioware/news/urls.py
+++ fanbioware/news/urls.py
@@ -6,11 +6,16 @@ app_name = 'news'
 urlpatterns = [
     path(
         'news/',
-        views.NewsList.as_view(),
+        views.NewsListView.as_view(),
         name='news_list'
     ),
     path(
-        'news/<int:news_pk>/',
+        'news/<slug:category_slug>/',
+        views.CategoryNewsListView.as_view(),
+        name='category_news_list'
+    ),
+    path(
+        'news/<slug:game_slug>/<int:news_pk>/',
         views.NewsDetailView.as_view(),
         name='news_detail'
     )
