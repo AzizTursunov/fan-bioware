0063549af19c675323ebbd9eed6d00de08da4cca
Aziz Tursunov <az.tursunov@gmail.com>
2022-05-19 14:58:52 +0300

    Configure admin for News model.

--- fanbioware/news/admin.py
+++ fanbioware/news/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from news.models import News
 
-# Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'title', 'get_html_photo', 'intro',
+        'pub_date', 'game',
+        'is_publicated'
+    )
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'intro', 'text')
+    list_editable = ('is_publicated',)
+    list_filter = ('game', )
+    readonly_fields = ('get_html_photo',)
+    save_on_top = True
+
+    def get_html_photo(self, object):
+        """"Return img html tag with image url"""
+        return format_html(f'<img src="{object.image.url}" width=100 height=70>')
+
+    get_html_photo.short_description = 'Cover image'
+
+
+admin.site.register(News, NewsAdmin)
